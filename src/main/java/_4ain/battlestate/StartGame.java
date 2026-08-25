0500816b8c2023a3641fd81f83a3c7a1b80e351b
Adrian <adrian.buruiana@edu.iisleviponti.it>
2026-08-25 19:45:42 +0200

    Minor Changes

--- src/main/java/_4ain/battlestate/StartGame.java
+++ src/main/java/_4ain/battlestate/StartGame.java
@@ -12,7 +12,7 @@ public class StartGame extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartGame.class.getResource("start-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 721);
-        stage.setTitle("Hello!");
+        stage.setTitle("BattleState");
         stage.setScene(scene);
         stage.show();
     }
